593635439c43b4db09971ec97e572216bf37d873
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2018-02-16 12:19:17 +0100

    proper activations in ffnn controller for ntm

--- models/run_experiment.py
+++ models/run_experiment.py
@@ -159,6 +159,7 @@ if __name__ == '__main__':
         'write_heads': 1,
         'shift_range': 3,
         'activation': 'softmax',  # for classification
+        'hidden_units': args.hidden_units
         'controller': 'ffnn'
     }
 
